6cecc1076d78a6199a7377b0f6a1be46e54e26a6
Aerixxxxss <ilhamm9074@gmail.com>
2026-01-16 03:10:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
